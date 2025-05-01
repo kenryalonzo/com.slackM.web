f62f48b7f7b1543b99947ca1c20e05152d58f4ee
FOTIE FOTIE NATHANAEL <kenryalonzo@gmail.com>
2025-05-01 15:21:08 +0100

    modification fonction du stream

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -64,9 +64,43 @@ export async function signup(req, res) {
 }
 
 export async function login(req, res) {
-    res.send("Login Route");
+     try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: "Invalid email or password" });
+        }
+        const isPasswordCorrect = await user.matchPassword(password);
+        if (!isPasswordCorrect) {
+            return res.status(400).json({ message: "Invalid email or password" });
+        }
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+        res.cookie("jwt", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+            maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+        });
+
+        res.status(200).json({ success: true, user});
+
+    } catch (error) {
+        
+        console.log("Error during login controller:", error.message);
+        res.status(500).json({ message: "Internal server error" });
+    }
 }
 
 export function logout(req, res) {
-    res.send("Logout Route");
+    res.clearCookie("jwt", {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+    });
+    res.status(200).json({ success: true, message: "Logged out successfully" });
 }
