9df42923e0369129af6d85e9190a30ce318aa915
FOTIE FOTIE NATHANAEL <kenryalonzo@gmail.com>
2025-05-01 19:14:56 +0100

    fix: file structure modified

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -9,4 +9,10 @@ router.get("/login", login);
 
 router.get("/logout", logout);
 
+router.post("/onboarding", protectRoute, onboard);
+
+router.get("/me", protectRoute, (req, res) => {
+  res.status(200).json({ success: true, message: "Onboarding successful" });
+});
+
 export default router;
