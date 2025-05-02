06cd5e11e174a9ad88e86bd711d6c570db99b71a
FOTIE FOTIE NATHANAEL <kenryalonzo@gmail.com>
2025-05-02 06:21:28 +0100

    libraries imported

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser';
 
 import authRoutes from './routes/auth.route.js'
+import userRoutes from './routes/user.route.js'
+
 import { connectDB } from './lib/db.js';
 
 dotenv.config()
